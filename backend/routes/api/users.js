8af98b17d955442a53ce2c8bbb11aac35bb5a54b
Jaden <jadenadams329@gmail.com>
2025-01-05 10:52:46 -0800

    backend boilerplate code is setup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,37 +1,58 @@
-const express = require('express');
-const bcrypt = require('bcryptjs');
+const express = require("express");
+const bcrypt = require("bcryptjs");
 
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User } = require('../../db/models');
+const { setTokenCookie, requireAuth } = require("../../utils/auth");
+const { User } = require("../../db/models");
 
 const router = express.Router();
 
+const { check } = require("express-validator");
+const { handleValidationErrors } = require("../../utils/validation");
+
+const validateSignup = [
+	check("email").exists({ checkFalsy: true }).isEmail().withMessage("Please provide a valid email."),
+	check("username")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 4 })
+		.withMessage("Please provide a username with at least 4 characters."),
+	check("password")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 6 })
+		.withMessage("Password must be 6 characters or more."),
+	check("name")
+		.exists({ checkFalsy: true })
+		.isLength({ min: 2 })
+		.withMessage("Please provide a name with at least 2 characters."),
+	check("role").exists({ checkFalsy: true }).withMessage("Role must be assigned"),
+	handleValidationErrors,
+];
+
 //Sign up
-router.post("/", async (req, res) => {
-    const { email, password, username, name, role } = req.body;
+router.post("/", validateSignup, async (req, res) => {
+	const { email, password, username, name, role } = req.body;
 
-    const hashedPassword = bcrypt.hashSync(password);
-    const user = await User.create({
-        username,
-        email,
-        hashedPassword,
-        name,
-        role
-    });
+	const hashedPassword = bcrypt.hashSync(password);
+	const user = await User.create({
+		username,
+		email,
+		hashedPassword,
+		name,
+		role,
+	});
 
-    const safeUser = {
-        id: user.id,
-        username: user.username,
-        email: user.email,
-        name: user.name,
-        role: user.role
-    }
+	const safeUser = {
+		id: user.id,
+		username: user.username,
+		email: user.email,
+		name: user.name,
+		role: user.role,
+	};
 
-    await setTokenCookie(res, safeUser);
+	await setTokenCookie(res, safeUser);
 
-    return res.json({
-        user: safeUser,
-    });
-})
+	return res.json({
+		user: safeUser,
+	});
+});
 
 module.exports = router;
